d0efffb95444f2676cdddda7ebf65a76c2c6bd3b
guadalupesilva1 <guadita.s05@gmail.com>
2026-04-05 21:15:21 -0300

    procesamiento_datos

---
+++ procesamiento_datos.py
@@ -0,0 +1,17 @@
+def filtrar_por_participante(datos, id_participante):
+    """
+    Qué hace la función:
+    Busca y devueleve el diccionario que contiene los datos de un participante en específico.
+
+    Parámetros:
+    datos (list): La lista que contiene los diccionarios de todos los participantes.
+    id_participante (int): El número de id del participante a filtrar.
+
+    Retorna:
+    dict: El diccionario con los datos de participante requerido.
+    """
+    for participante in datos:
+        if participante["id_participante"] == id_participante:
+            datos_participante = participante
+            break
+    return datos_participante
